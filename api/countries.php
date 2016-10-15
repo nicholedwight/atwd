352fa600b3e9b429dd1056a2fabb0405886fb244
Nichole Dwight <nicholedwight@gmail.com>
2016-10-15 16:16:29 +0100

    adding countries array

---
+++ api/countries.php
@@ -0,0 +1,21 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$country_by_currency_url = 'https://restcountries.eu/rest/v1/currency/';
+$currency = 'USD';
+$endpoint = $country_by_currency_url . $currency;
+$request = file_get_contents($endpoint);
+
+$responseArray = json_decode($request, true);
+
+$countries = array();
+
+for ($i = 0; $i < count($responseArray); $i++ ) {
+    array_push($countries, $responseArray[$i]["name"]);
+}
+
+echo '<pre>';
+var_dump($countries);
+echo '</pre>';
--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('watch', ['images', 'styles', 'scripts'], function() {
 });
 
 gulp.task('styles', function(cb) {
-  return gulp.src('./assets/scss')
+  return gulp.src('./assets/css')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/assets/css'))
     .pipe(browserSync.stream());
