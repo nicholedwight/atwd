3646f49c6b79bb03766d01c55a185c42240d744c
Nichole Dwight <nicholedwight@gmail.com>
2016-10-17 15:06:22 +0100

    split into two functions, called on index - working

--- api/currencies.php
+++ api/currencies.php
@@ -1,36 +1,51 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+function getLocations($code) {
+  global $result;
 
-include('countries.php');
+  $country_by_currency_url = 'https://restcountries.eu/rest/v1/currency/';
+  $endpoint = $country_by_currency_url . $code;
+  $request = file_get_contents($endpoint);
 
-$API_KEY = "18947b8fdda74706b676b4ab92faa09d";
-$exchange_rate_url = 'https://openexchangerates.org/api/latest.json?app_id=';
+  $responseArray = json_decode($request, true);
 
-$json_data = file_get_contents($exchange_rate_url  . $API_KEY);
-$data = json_decode($json_data, true);
-$rates = array_slice($data, 4);
-$xml = new SimpleXMLElement('<currencies/>');
+  $countries = array();
 
-
-echo "<pre>";
-var_dump($rates);
-echo "</pre>";
-
-
-
-
-foreach($rates as $keys => $values) {
-  foreach($values as $key => $value) {
-    getLocations($key);
-    $currency = $xml->addChild('currency');
-    $currency->addAttribute('rate', $value);
-    $currency->addAttribute('code', $key);
-    $currency->addChild('name');
-    $currency->addChild('locations', $result);
+  for ($i = 0; $i < count($responseArray); $i++ ) {
+      array_push($countries, $responseArray[$i]["name"]);
   }
+
+  $result = implode(", ", $countries);
+
+  return $result;
 }
 
-// Header('Content-type: text/xml');
-$xml->asXML("../data/currencies.xml");
+function getCurrencies() {
+  global $result;
+
+  $API_KEY = "18947b8fdda74706b676b4ab92faa09d";
+  $exchange_rate_url = 'https://openexchangerates.org/api/latest.json?app_id=';
+
+  $json_data = file_get_contents($exchange_rate_url  . $API_KEY);
+  $data = json_decode($json_data, true);
+  $rates = array_slice($data, 4);
+  $xml = new SimpleXMLElement('<currencies/>');
+
+
+  // echo "<pre>";
+  // var_dump($rates);
+  // echo "</pre>";
+
+  foreach($rates as $keys => $values) {
+    foreach($values as $key => $value) {
+      getLocations($key);
+      $currency = $xml->addChild('currency');
+      $currency->addAttribute('rate', $value);
+      $currency->addAttribute('code', $key);
+      $currency->addChild('name');
+      $currency->addChild('locations', $result);
+    }
+  }
+
+  // Header('Content-type: text/xml');
+  $xml->asXML("./data/currencies.xml");
+}
