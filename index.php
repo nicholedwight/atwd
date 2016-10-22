385f79ed6622074e5eac26add9fd568cd932cc13
Nichole Dwight <nicholedwight@gmail.com>
2016-10-22 14:06:36 +0100

    removing commented failed code

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ include('api/currencies.php');
 require('api/errors.php');
 // getCurrencies();
 //
-// if (!empty($_GET['amnt'])) {
-//   $xml = simplexml_load_file('./data/currencies.xml');
-//   echo $xml->currency[0]->name;
-// } else {
-//   $xml = simplexml_load_string($missingParameter);
-//   Header('Content-type: text/xml');
-//   echo $xml->asXML();
-// }
+if (!empty($_GET['amnt'])) {
+  $xml = simplexml_load_file('./data/currencies.xml');
+  echo $xml->currency[0]->name;
+} else {
+  $xml = simplexml_load_string($missingParameter);
+  Header('Content-type: text/xml');
+  echo $xml->asXML();
+}
 
 if (!empty($_GET['from'])) {
   $request = (string)$_GET['from'];
@@ -33,31 +33,6 @@ if (!empty($_GET['from'])) {
     Header('Content-type: text/xml');
     echo $errors->asXML();
   }
-  // foreach($xml->currency as $currency) {
-  //    $currency->name;
-    // if($currency['code'] == $request) {
-    //   echo $request . ' is the same as ' . $currency['code'];
-    //   break;
-    // } else {
-    //   // echo "fail";
-    //   // break;
-    //   // $errors = simplexml_load_string($currencyNotRecognised);
-    //   // Header('Content-type: text/xml');
-    //   // echo $errors->asXML();
-    // }
-
-
-
-    // switch ($currency) {
-    //   case ['code'] == $request;
-    //     echo $request;
-    //     break;
-    //   case ['code'] != $request;
-    //     $errors = simplexml_load_string($currencyNotRecognised);
-    //     Header('Content-type: text/xml');
-    //     echo $errors->asXML();
-    // }
-  // }
 
 } else {
 
