97986fc6af081403c333bf301a06521bf78eb410
Nichole Dwight <nicholedwight@gmail.com>
2016-10-22 16:12:24 +0100

    basic form submitting data to convert file, no working conversions yet, still working on errors

--- index.php
+++ index.php
@@ -8,48 +8,16 @@ require('api/errors.php');
 getCurrencyCodes();
 // getCurrencies();
 //
-// if (!empty($_GET['amnt'])) {
-//   $xml = simplexml_load_file('./data/currencies.xml');
-//   echo $xml->currency[0]->name;
-// } else {
-//   $xml = simplexml_load_string($missingParameter);
-//   Header('Content-type: text/xml');
-//   echo $xml->asXML();
-// }
 
-if (!empty($_GET['from'])) {
-  $from = (string)$_GET['from'];
-  $xml = simplexml_load_file('./data/currencies.xml');
-  $match = false;
-  for($i = 0; $i < count($xml); $i++) {
-    if($xml->currency[$i]['code'] == $from) {
-      echo $i;
-      echo 'success';
-      $match = true;
-      return;
-    }
-  }
-
-  if ($match == false ) {
-    $errors = simplexml_load_string($currencyNotRecognised);
-    Header('Content-type: text/xml');
-    echo $errors->asXML();
-    // $json = json_encode($errors);
-    // echo $json;
-  }
-
-} else {
-
-}
 ?>
 
-<form action="" method="POST">
-  <input type='text' id="amnt" />
-  <select id="from">
+<form action="convert.php" method="GET">
+  <input type='text' name="amnt" />
+  <select name="from">
     <?php foreach($codesArray as $codeName) { ?>
       <option id=""><?php echo $codeName;?> </option>
     <?php }?>
-  </select> to <select id="to">
+  </select> to <select name="to">
     <?php foreach($codesArray as $codeName) { ?>
       <option id=""><?php echo $codeName;?> </option>
     <?php }?>
