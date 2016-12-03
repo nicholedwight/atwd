90337ebfdaa67de6f53b40bdcbb9ca88d6f6aae5
Nichole Dwight <nicholedwight@gmail.com>
2016-12-03 16:49:04 +0000

    i think error messages are working

--- config.php
+++ config.php
@@ -38,21 +38,6 @@ $frmdelete = array('code');
 
 $update_interval = 43200;
 
-# error_hash to hold error numbers and messages
-$error_hash = array(
-	1000 => 'Currency type not recognized',
-	1100 => 'Required parameter is missing',
-	1200 => 'Parameter not recognized',
-	1300 => 'Currency amount mustbe a decimal number',
-	1400 => 'Error in service',
-	2000 => 'Method not recognized or is missing',
-	2100 => 'Rate in wrong format or is missing',
-	2200 => 'Currency code in wrong format or is missing',
-	2300 => 'Country name in wrong format or is missing',
-	2400 => 'Currency code not found for update',
-	2500 => 'Error in service'
-);
-
 # determine if local or UWE server
 if (stristr($_SERVER['HTTP_HOST'], 'local')) {
 	$local = TRUE;
@@ -60,28 +45,6 @@ if (stristr($_SERVER['HTTP_HOST'], 'local')) {
 	$local = FALSE;
 }
 
-# function to return formatted json or xml error msgs
-# expects three params - error_number, error_hash & format
-# if format missing, default to xml
-function generate_error($eno, $error_hash, $format='xml') {
-	$msg = $error_hash[$eno];
-
-	if ($format=='json') {
-		$json = array('conv' => array("code" => "$eno", "msg" => "$msg"));
-		header('Content-Type: application/json');
-		return json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-	}
-	else {
-		$xml =  '<?xml version="1.0" encoding="UTF-8"?>';
-		$xml .= '<conv><error>';
-		$xml .= '<code>' . $eno . '</code>';
-		$xml .= '<msg>' . $msg . '</msg>';
-		$xml .= '</error></conv>';
-
-		header('Content-type: text/xml');
-		return $xml;
-	}
-}
 
 
 # determine location of files and the URL of the site:
--- index.php
+++ index.php
@@ -5,10 +5,58 @@ require('config.php');
 // getCurrencies();
 // getCountries();
 
+require_once('generate_error.php');
+# currency code array
+# kept in the config file
+$ccodes = array(
+   'CAD','CHF','CNY','DKK',
+   'EUR','GBP','HKD','HUF',
+   'INR','JPY','MXN','MYR',
+   'NOK','NZD','PHP','RUB',
+   'SEK','SGD','THB','TRY',
+   'USD','ZAR');
+# parmeters in URL and format values expected
+# kept in the config file
+$params = array('from', 'to', 'amnt', 'format');
+$frmts = array('xml', 'json');
+# turn $_GET params into PHP variables
+extract($_GET);
+# set format to default to XML
+if (!isset($format)) {
+	$format = 'xml';
+}
+$get = array_intersect($params, array_keys($_GET));
+if (count($get) < 4) {
+	echo generate_error(1100, $error_hash, $format);
+	exit;
+}
+if (count($_GET) > 4) {
+	echo generate_error(1200, $error_hash, $format);
+	exit;
+}
+# $to and $from are not recognized currencies
+if (!in_array($to, $ccodes) || !in_array($from, $ccodes)) {
+	echo generate_error(1000, $error_hash, $format);
+	exit;
+}
+# check for allowed format values
+if (!in_array($format, $frmts)) {
+	echo generate_error(1200, $error_hash, $format);
+	exit;
+}
+# $amnt is not a decimal value
+if (!preg_match('/^[+-]?(\d*\.\d+([eE]?[+-]?\d+)?|\d+[eE][+-]?\d+)$/', $amnt)) {
+	echo generate_error(1300,  $error_hash, $format);
+	exit;
+}
+# now read in data files
+# update rate if more than 12 hours old
+# do conversion
+# echo result as XML or JSON depending on format param.
 
 
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <body>
   <form action="convert.php" method="GET">
@@ -25,4 +73,4 @@ require('config.php');
     <button type="submit" name="submit">Submit</button>
   </form>
 </body>
-</html>
+</html> -->
